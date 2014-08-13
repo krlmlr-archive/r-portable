b6ac24d219098071e839abe6e486ad445f66df22
Kirill Müller <krlmlr@mailbox.org>
2014-08-13 15:42:01 -0700

    basic unpack functionality

--- Library.ps1
+++ Library.ps1
@@ -4,3 +4,14 @@ Function Download {
 
     Invoke-WebRequest http://cran.r-project.org/bin/windows/base/R-devel-win.exe -OutFile .\DL\R-devel-win.exe
 }
+
+Function Unpack {
+    [CmdletBinding()]
+    Param()
+
+    rm -Recurse -Force .\Image
+    md .\Image
+    .\Tools\innounp\innounp.exe -x -dImage .\DL\R-devel-win.exe
+    mv ".\Image\{app}" .\Image\R
+    rm .\Image\install_script.iss
+}

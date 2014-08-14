20a54ab9367aa2d71e74422ae9d9aeba10e0988d
Kirill Müller <krlmlr@mailbox.org>
2014-08-14 00:15:24 -0700

    avoid stop due to warning messages by Rscript

--- Library.ps1
+++ Library.ps1
@@ -1,3 +1,20 @@
+# Found at http://zduck.com/2012/powershell-batch-files-exit-codes/
+Function Exec
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Position=0, Mandatory=1)]
+        [scriptblock]$Command,
+        [Parameter(Position=1, Mandatory=0)]
+        [string]$ErrorMessage = "Execution of command failed.`n$Command"
+    )
+    $ErrorActionPreference = "Continue"
+    & $Command 2>&1 | %{ "$_" }
+    if ($LastExitCode -ne 0) {
+        throw "Exec: $ErrorMessage`nExit code: $LastExitCode"
+    }
+}
+
 Function Download {
     [CmdletBinding()]
     Param()
@@ -22,7 +39,7 @@ Function Unpack {
     #.\Tools\innounp\innounp.exe -x -dImage .\DL\Rtools-current.exe
     #mv ".\Image\{app}" .\Image\Rtools
 
-    .\Image\R\bin\i386\Rscript -e "install.packages(commandArgs(TRUE), repos='http://cran.r-project.org')" devtools testthat
+    Exec { .\Image\R\bin\i386\Rscript.exe -e "install.packages(commandArgs(TRUE), repos='http://cran.r-project.org')" devtools testthat }
 }
 
 Function CreateImage {

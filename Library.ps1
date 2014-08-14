9fb87fe7480d71ca8d84f93528c1ea556dcf6768
Kirill Müller <krlmlr@mailbox.org>
2014-08-14 02:33:29 +0100

    download Rtools, but don't extract yet -- don't know yet where to extract

--- Library.ps1
+++ Library.ps1
@@ -3,6 +3,9 @@ Function Download {
     Param()
 
     Invoke-WebRequest http://cran.r-project.org/bin/windows/base/R-devel-win.exe -OutFile .\DL\R-devel-win.exe
+    $rtoolsver = $(Invoke-WebRequest http://cran.r-project.org/bin/windows/Rtools/VERSION.txt).Content.Split(' ')[2].Split('.')[0..1] -Join ''
+    $rtoolsurl = "http://cran.r-project.org/bin/windows/Rtools/Rtools$rtoolsver.exe"
+    Invoke-WebRequest $rtoolsurl -OutFile "DL\Rtools-current.exe"
 }
 
 Function Unpack {
@@ -11,9 +14,13 @@ Function Unpack {
 
     #rm -Recurse -Force .\Image
     md .\Image
+
     .\Tools\innounp\innounp.exe -x -dImage .\DL\R-devel-win.exe
     mv ".\Image\{app}" .\Image\R
     rm .\Image\install_script.iss
+
+    #.\Tools\innounp\innounp.exe -x -dImage .\DL\Rtools-current.exe
+    #mv ".\Image\{app}" .\Image\Rtools
 }
 
 Function CreateImage {

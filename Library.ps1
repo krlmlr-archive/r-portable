5c1a0bd4f120c627bffc65da9730014348f5e7b8
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-08-15 13:51:09 +0200

    write logs to current directory, perhaps this helps deploying them

--- Library.ps1
+++ Library.ps1
@@ -47,17 +47,17 @@ Function Unpack {
 
     # R
     Progress "Extracting R"
-    .\Tools\innounp\innounp.exe -x -dImage .\DL\R-devel-win.exe > ..\R-devel-win.log
+    .\Tools\innounp\innounp.exe -x -dImage .\DL\R-devel-win.exe > .\R-devel-win.log
     mv ".\Image\{app}" .\Image\R
     rm .\Image\install_script.iss
 
     # R packages devtools and testthat
     Progress "Installing additional packages"
-    Exec { .\Image\R\bin\i386\Rscript.exe -e "install.packages(commandArgs(TRUE), repos='http://cran.r-project.org')" devtools testthat knitr } > ..\R-packages.log
+    Exec { .\Image\R\bin\i386\Rscript.exe -e "install.packages(commandArgs(TRUE), repos='http://cran.r-project.org')" devtools testthat knitr } > .\R-packages.log
 
     # Rtools
     Progress "Extracting Rtools"
-    .\Tools\innounp\innounp.exe -x -dImage .\DL\Rtools-current.exe > ..\Rtools-current.log
+    .\Tools\innounp\innounp.exe -x -dImage .\DL\Rtools-current.exe > .\Rtools-current.log
     mv ".\Image\{app}" .\Image\Rtools
     rm .\Image\install_script.iss
     # Don't seem to need those to build packages -- only to build R
